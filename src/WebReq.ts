687833033475f57c9c4238516905396b5332cc04
Manrit Kaur <manritkaur3923@gmail.com>
2026-03-03 18:01:12 +0530

    request using Axios and Fetch

---
+++ src/WebReq.ts
@@ -0,0 +1,25 @@
+import axios, { isAxiosError } from "axios";
+import type { AxiosResponse } from "axios";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const fetchData = async () => {
+  try {
+    const response: AxiosResponse<Todo> = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    console.log("Todo", response.data)
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.message);
+      if (error.response) {
+        console.log(error.response.status);
+      }
+    }
+  }
+};
